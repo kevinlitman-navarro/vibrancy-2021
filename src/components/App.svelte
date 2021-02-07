acff181324d08b55f90bd5371dd225e54b46a4be
kevinlitman-navarro <kevin.litnav@gmail.com>
2021-02-07 11:17:01 -0800

    Initial commit

---
+++ src/components/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Meta from "./Meta.svelte";
+  import Window from "./helpers/Window.svelte";
+  import Header from "./pudding/Header.svelte";
+  import Demo from "./demo/Demo.svelte";
+  import Footer from "./pudding/Footer.svelte";
+</script>
+
+<Meta />
+
+<Window />
+
+<Header />
+
+<Demo />
+
+<Footer />
